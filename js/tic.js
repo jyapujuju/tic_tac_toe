e300aeb390b0547fd6bd1cf28d3200ff8e41f42c
jyapujuju <danieldangol@gmail.com>
2017-12-01 13:22:12 +0545

    count wins, show score, show turn

--- js/tic.js
+++ js/tic.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   var count = 0;
+  var P1_score = 0;
+  var P2_score = 0;
   $(".button").click(function(){
     if($(this).val() === ''){
       if(count%2 == 0){
         $(this).text("X");
-        $(this).val("X");
-        console.log(count);
+        $(this).val("P1");
+        $("#turn").text('P2 turn');
       }
       else {
         $(this).text("O");
-        $(this).val("O");
-        console.log(count);
+        $(this).val("P2");
+        $("#turn").text('P1 turn');
       }
     count++;
     if (count>2){
@@ -23,9 +25,14 @@ $(document).ready(function() {
       ($("#1").val() === $("#4").val() && $("#1").val() === $("#7").val() || //check whether button 1,4,7 are equal or first column is same
       $("#1").val() === $("#2").val() && $("#1").val() === $("#3").val()) && //check whether button 1,2,3 are equal or first row is same
       $("#1").val() !== '' ){
-        console.log($("#1").val() + ' win');
+      $('#1').val() === "P1" ? P1_score++ : P2_score++;
+      $("#result_p1").text("P1 win " + P1_score + " game");
+      $("#result_p2").text("P2 win " + P2_score + " game");
+      $("#score").text($("#1").val() + ' win');
+      setTimeout(function(){
         reload_div();
-        count = 0;
+      }, 500);
+      count = 0;
     }
     else if (
       (($("#2").val() === $("#5").val() && $("#5").val() === $("#8").val()) ||
@@ -33,28 +40,29 @@ $(document).ready(function() {
       ($("#1").val() === $("#5").val() && $("#5").val() === $("#9").val()) ||
       ($("#7").val() === $("#5").val() && $("#5").val() === $("#3").val())) && //check whether button 3,5,7 are equal or diagonal is same
       $("#5").val() !== '' ) {
-        console.log($("#5").val() + ' win');
-        reload_div();
-        count = 0;
+      $("#score").text($("#5").val() + ' win').delay( 800 );
+      $('#1').val()==='P1'? P1_score++ : P2_score++;
+      reload_div();
+      count = 0;
     }
     else if (
       ($("#3").val() === $("#6").val() && $("#3").val() === $("#9").val()) && //check whether button 3,6,9 are equal or third column is same
       $("#6").val() !== '') {
-        console.log($("#3").val() + ' win');
-        reload_div();
-        count = 0;
+      $("#score").text($("#3").val() + ' win');
+      reload_div();
+      count = 0;
     }
     else if (
       ($("#7").val() === $("#8").val() && $("#7").val() === $("#9").val()) && //check whether button 7,8,9 are equal or third row is same
       $("#7").val() !== '') {
-        console.log($("#7").val() + ' win');
-        reload_div();
-        count = 0;
+      $("#score").text($("#7").val() + ' win');
+      reload_div();
+      count = 0;
     }
     else if ( count < 9){
     }
     else {
-      console.log("tie");
+      $("#score").text('tie');
       reload_div();
       count = 0;
     }
@@ -78,5 +86,9 @@ $(document).ready(function() {
     $("#8").text('-');
     $("#9").val('');
     $("#9").text('-');
+    $("#turn").text('P1 turn');
+    $("#score").empty();
+
   }
+
 });
--- readme.md
+++ readme.md
@@ -2,8 +2,9 @@
 check  [this](https://jyapujuju.github.io/tic_tac_toe/) online now
 ### todo
 - [x] reset dom after win or tie
-- [ ] show result in div not in console
-- [ ] center the content
-- [ ] add title
-- [ ] say who is who ? example player 1 is x
-- [ ] count wins
+- [x] show result in div not in console
+- [x] center the content
+- [x] add title
+- [x] say who is who ? example player 1 is x
+- [x] count wins
+- [ ] style
