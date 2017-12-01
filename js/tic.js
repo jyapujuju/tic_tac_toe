dce08b79a03fd34e17b9f6700e9d1b45563c5021
jyapujuju <danieldangol@gmail.com>
2017-12-01 14:49:46 +0545

    some design

--- js/tic.js
+++ js/tic.js
@@ -25,13 +25,10 @@ $(document).ready(function() {
       ($("#1").val() === $("#4").val() && $("#1").val() === $("#7").val() || //check whether button 1,4,7 are equal or first column is same
       $("#1").val() === $("#2").val() && $("#1").val() === $("#3").val()) && //check whether button 1,2,3 are equal or first row is same
       $("#1").val() !== '' ){
-      $('#1').val() === "P1" ? P1_score++ : P2_score++;
-      $("#result_p1").text("P1 win " + P1_score + " game");
-      $("#result_p2").text("P2 win " + P2_score + " game");
-      $("#score").text($("#1").val() + ' win');
+      logic("#1");
       setTimeout(function(){
         reload_div();
-      }, 500);
+      }, 1000);
       count = 0;
     }
     else if (
@@ -39,31 +36,38 @@ $(document).ready(function() {
       ($("#4").val() === $("#5").val() && $("#5").val() === $("#6").val()) ||
       ($("#1").val() === $("#5").val() && $("#5").val() === $("#9").val()) ||
       ($("#7").val() === $("#5").val() && $("#5").val() === $("#3").val())) && //check whether button 3,5,7 are equal or diagonal is same
-      $("#5").val() !== '' ) {
-      $("#score").text($("#5").val() + ' win').delay( 800 );
-      $('#1').val()==='P1'? P1_score++ : P2_score++;
-      reload_div();
+      $("#5").val() !== '' ){
+      logic("#5");
+      setTimeout(function(){
+        reload_div();
+      }, 1000);
       count = 0;
     }
     else if (
       ($("#3").val() === $("#6").val() && $("#3").val() === $("#9").val()) && //check whether button 3,6,9 are equal or third column is same
       $("#6").val() !== '') {
-      $("#score").text($("#3").val() + ' win');
-      reload_div();
+      logic("#3");
+      setTimeout(function(){
+        reload_div();
+      }, 1000);
       count = 0;
     }
     else if (
       ($("#7").val() === $("#8").val() && $("#7").val() === $("#9").val()) && //check whether button 7,8,9 are equal or third row is same
       $("#7").val() !== '') {
-      $("#score").text($("#7").val() + ' win');
-      reload_div();
+      loginc("#7");
+      setTimeout(function(){
+        reload_div();
+      }, 1000);
       count = 0;
     }
-    else if ( count < 9){
+    else if (count < 9){
     }
     else {
-      $("#score").text('tie');
-      reload_div();
+      $("#turn").text('tie').css("color","red");
+      setTimeout(function(){
+        reload_div();
+      }, 1000);
       count = 0;
     }
   }
@@ -86,9 +90,15 @@ $(document).ready(function() {
     $("#8").text('-');
     $("#9").val('');
     $("#9").text('-');
-    $("#turn").text('P1 turn');
+    $("#turn").text('P1 turn').css("color","black");
     $("#score").empty();
-
+  }
+  function logic(button_val){
+    $(button_val).val() === "P1" ? P1_score++ : P2_score++;
+    $("#result_p1").text("P1 win " + P1_score + " game");
+    $("#result_p2").text("P2 win " + P2_score + " game");
+    // $("#score").text($("#1").val() + ' win');
+    $("#turn").css("color","Lime").text($(button_val).val() + ' win');
   }
 
 });
